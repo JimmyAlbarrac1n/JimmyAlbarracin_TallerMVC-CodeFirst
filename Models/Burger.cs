ae6d326ce9b2cf228b49f3eab23e92399f03ccbe
laboratorio <laboratorio@UPOAULA10302>
2024-04-10 11:06:46 -0500

    Commit 1, clase Burger.cs creada

---
+++ Models/Burger.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JimmyAlbarracin_TallerMVC_CodeFirst.Models
+{
+    public class Burger
+    {
+        public int Id { get; set; } //ID obligatorio 
+        [Required]//campo obligatorio
+        public string? Name { get; set; } //? en el string sirve para contener valores nulos
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]//validaciones para la base de datos
+        public decimal Precio { get; set; }
+    }
+}
